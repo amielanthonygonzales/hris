f54983174010cf9f5c6554f5c3d88e546474d4c3
amielanthonygonzales <amielanthonygonzales4@gmail.com>
2018-04-07 19:10:45 +0800

    pagibig form with label

--- application/views/app/content/page-pagibig-contri.php
+++ application/views/app/content/page-pagibig-contri.php
@@ -5,7 +5,35 @@
 		</div>
 		<div class="panel-body">
 			<div class="form-pagibig">
-				<input type="text"/>
+				<input type="text" class="employerId" />
+				<input type="text" class="businessName" />
+				<input type="text" class="branch" />
+				<input type="text" class="roomNo" />
+				<input type="text" class="buildingName" />
+				<input type="text" class="blockNo" />
+				<input type="text" class="streetName" />
+				<input type="text" class="subdivision" />
+				<input type="text" class="barangay" />
+				<input type="text" class="city" />
+				<input type="text" class="province" />
+				<input type="text" class="zipCode" />
+				<input type="text" class="periodCovered" />
+				<input type="text" class="midNo1" />
+				<input type="text" class="name1" />
+				<input type="text" class="accountNo1" />
+				<input type="text" class="monthly1" />
+				<input type="text" class="employeeShare1" />
+				<input type="text" class="employerShare1" />
+				<input type="text" class="total1" />
+				<input type="text" class="remarks1" />
+				<input type="text" class="midNo2" />
+				<input type="text" class="name2" />
+				<input type="text" class="accountNo2" />
+				<input type="text" class="monthly2" />
+				<input type="text" class="employeeShare2" />
+				<input type="text" class="employerShare2" />
+				<input type="text" class="total2" />
+				<input type="text" class="remarks2" />
 			</div>
 			<div class="print-form-pagibig"> Print this form</div>
 		</div>
